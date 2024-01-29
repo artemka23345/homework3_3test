73ad00ca94f9573d019c44644be495a1a9041e25
artemka314 <tarabakin.artem21@gmail.com>
2024-01-29 12:50:19 +0300

    Initial commit

---
+++ src/main/java/com/skypro/homework3_3test/Homework33testApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.homework3_3test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework33testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework33testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/homework3_3test/Homework33testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.homework3_3test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework33testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
