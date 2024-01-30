4cd94a09a80cda5d035046dd627a52892f42af0d
Inv <artem@tarabakin.ru>
2024-01-30 10:46:17 +0300

    add exeptions

--- src/main/java/com/skypro/homework3_3test/controller/CalculatorController.java
+++ src/main/java/com/skypro/homework3_3test/controller/CalculatorController.java
@@ -1,7 +1,6 @@
 package com.skypro.homework3_3test.controller;
 
 import com.skypro.homework3_3test.service.CalculatorService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +15,10 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
+    @GetMapping
+    public String hello(){
+        return "Добро пожаловать в калькулятор";
+    }
     @GetMapping("/plus")
     public Integer plus(Integer num1,Integer num2){
         return calculatorService.plus(num1,num2);
---
+++ src/main/java/com/skypro/homework3_3test/exeption/IllegalArgumentExceptionDivide.java
@@ -0,0 +1,7 @@
+package com.skypro.homework3_3test.exeption;
+
+public class IllegalArgumentExceptionDivide extends IllegalArgumentException {
+    public IllegalArgumentExceptionDivide(String s) {
+        super(s);
+    }
+}
