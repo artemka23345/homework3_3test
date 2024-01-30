e35746ea36a5c605ba2820240c80eea59b694565
Inv <artem@tarabakin.ru>
2024-01-30 12:29:16 +0300

    Corrected

--- src/main/java/com/skypro/homework3_3test/controller/CalculatorController.java
+++ src/main/java/com/skypro/homework3_3test/controller/CalculatorController.java
@@ -3,6 +3,7 @@ package com.skypro.homework3_3test.controller;
 import com.skypro.homework3_3test.service.CalculatorService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,19 +21,19 @@ public class CalculatorController {
         return calculatorService.hello();
     }
     @GetMapping("/plus")
-    public String plus(Integer num1, Integer num2){
+    public String plus(@RequestParam Integer num1,@RequestParam Integer num2){
         return num1 + "+" + num2 + "=" + calculatorService.plus(num1,num2);
     }
     @GetMapping("/minus")
-    public String minus(Integer num1, Integer num2){
+    public String minus(@RequestParam Integer num1,@RequestParam Integer num2){
         return num1 + "-" + num2 + "=" + calculatorService.minus(num1,num2);
     }
     @GetMapping("/multiply")
-    public String multiply(Integer num1, Integer num2){
+    public String multiply(@RequestParam Integer num1,@RequestParam Integer num2){
         return num1 + "*" + num2 + "=" + calculatorService.multiply(num1,num2);
     }
     @GetMapping("/divide")
-    public String divide(Double num1, Double num2){
+    public String divide(@RequestParam Double num1,@RequestParam Double num2){
         return num1 + "/" + num2 + "=" + calculatorService.divide(num1,num2);
     }
 
